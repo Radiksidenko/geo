4148b62e4cfdea14cb84f39ef6d0ef725442ee1f
Andrew <yrtsky@gmail.com>
2017-12-10 22:25:41 +0200

    add profile edit

--- assets/app/user-profile/user-profile.component.js
+++ assets/app/user-profile/user-profile.component.js
@@ -20,13 +20,41 @@ System.register(["@angular/core"], function (exports_1, context_1) {
         execute: function () {
             UserProfileComponent = class UserProfileComponent {
                 constructor() {
+                    this.editing = false;
                     var reference = this;
                     io.socket.get('/getuser_me', function gotResponse(body, response) {
                         reference.name = body.name;
+                        reference.surname = body.surname;
+                        reference.gender = body.gender;
+                        reference.date_of_birth = body.date_of_birth;
+                        reference.country = body.country;
+                        reference.city = body.city;
+                        reference.phone = body.phone;
+                        reference.web_site = body.web_site;
+                        reference.interests = body.interests;
+                        reference.about_myself = body.about_myself;
+                        reference.role = body.role;
                         reference.email = body.email;
-                        reference.id = body.id;
+                        reference.password = body.password;
                         reference.lastLoggedIn = body.lastLoggedIn;
                         reference.gravatarUrl = body.gravatarUrl;
+                    });
+                    io.socket.get("/ready", function (ready) { });
+                    io.socket.on('connect', function () {
+                        console.log('Connect');
+                    });
+                    io.socket.on('reconnect', function () {
+                        console.log('Reconnect');
+                    });
+                    io.socket.on('disconnect', function () {
+                        console.log('Disconnect');
+                    });
+                    setInterval(function () {
+                    }, 10);
+                }
+                update(type, value) {
+                    io.socket.post('/update', { [type]: value }, function (resData, jwRes) {
+                        console.log(jwRes.statusCode); // => 200
                     });
                 }
             };
--- assets/app/user-profile/user-profile.component.ts
+++ assets/app/user-profile/user-profile.component.ts
@@ -8,24 +8,68 @@ import {Component} from "@angular/core";
 
 export class UserProfileComponent {
 
-    name;
-    email;
-    id;
-    lastLoggedIn;
-    gravatarUrl;
+    name: string;
+    surname: string;
+    gender: string;
+    date_of_birth: Date;
+    country: string;
+    city: string;
+    phone: string;
+    web_site: string;
+    interests: string;
+    about_myself: string;
+    role: string;
+    email: string;
+    password: string;
+    lastLoggedIn: Date;
+    gravatarUrl: string;
 
+    editing: boolean = false;
+    
+    
     constructor(){
         var reference = this;
 
         io.socket.get('/getuser_me', function gotResponse(body, response) {
             reference.name = body.name;
+            reference.surname = body.surname;
+            reference.gender = body.gender;
+            reference.date_of_birth = body.date_of_birth;
+            reference.country = body.country;
+            reference.city = body.city;
+            reference.phone = body.phone;
+            reference.web_site = body.web_site;
+            reference.interests = body.interests;
+            reference.about_myself = body.about_myself;
+            reference.role = body.role;
             reference.email = body.email;
-            reference.id = body.id;
+            reference.password = body.password;
             reference.lastLoggedIn = body.lastLoggedIn;
             reference.gravatarUrl = body.gravatarUrl;
-
         });
 
+        io.socket.get("/ready", function (ready) {});
+
+        io.socket.on('connect', function () {
+            console.log('Connect');
+        });
+
+        io.socket.on('reconnect', function () {
+            console.log('Reconnect');
+        });
+
+        io.socket.on('disconnect', function () {
+            console.log('Disconnect');
+        });
+        setInterval(function () {
+        }, 10);
+        
+    }
+
+    update(type: string, value) {
+        io.socket.post('/update', {[type]: value}, function (resData, jwRes) {
+            console.log(jwRes.statusCode); // => 200
+        });
     }
 
 }
